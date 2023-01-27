f49be62269f015f7ef894d1f96a0950ef883e6cd
ambrisolla <andremuzel@gmail.com>
2023-01-27 17:50:28 -0300

    add conf files

---
+++ install-node-exporter.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e 
+
+01_install_prereqs() {
+	if test `egrep "ID=*.(ol|fedora)" /etc/os-release | wc -l` = "1"
+	then
+    yum install epel-release -y;
+    yum install git jq wget -y;
+	elif test `egrep "ID=*.(ubuntu|debian)" /etc/os-release | wc -l` = "1"
+	then
+		apt update;
+	  apt install git jq wget -y
+	else
+		echo "Not supported!"
+		exit 1
+	fi
+}
+
+02_download_node_exporter() {
+  NODE_EXPORTER_VERSION=$( \
+    curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest |\
+    jq -r .tag_name | sed "s/^v//g" )
+  NODE_EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
+  wget ${NODE_EXPORTER_URL} -O /tmp/node_exporter.tar.gz
+}
+
+03_install_node_exporter() {
+  useradd prometheus
+  mkdir -p /opt/node-exporter
+  cp node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /opt/node-exporter
+  chown prometheus:prometheus /opt/node-exporter -R
+  cp node-exporter.service /etc/systemd/system/
+  systemctl daemon-reload
+  systemctl enable node-exporter
+  systemctl start node-exporter
+}
+
+01_install_prereqs
+02_download_node_exporter
+03_install_node_exporter
